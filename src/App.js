e8f4770bb675b1d9259afa20bcebc4e27d798880
Daniel Malesevic <daniel.malesevic@gmx.at>
2020-01-09 16:24:03 +0000

    Add calculation for total

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { ProductCard } from './components/ProductCard';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ProductCard } from "./components/ProductCard";
 
 function App() {
-
   const [products, setProducts] = useState([]);
   const [total, setTotal] = useState(0);
 
   // fetch all items
   useEffect(() => {
     const fetchItems = async () => {
-      const res = await axios.get('https://my-json-server.typicode.com/benirvingplt/products/products');
+      const res = await axios.get(
+        "https://my-json-server.typicode.com/benirvingplt/products/products"
+      );
       setProducts(res.data);
-    }
+    };
     fetchItems();
-  }, [])
+  }, []);
 
   return (
     <div className="App container">
       {products.map(product => (
-        <ProductCard product={product} />
+        <ProductCard
+          product={product}
+          handleTotal={price => setTotal(Number(Math.abs(total + price).toFixed(2)))}
+        />
       ))}
+      <div className="card">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <p className="title is-3">Total</p>
+            </div>
+            <div className="media-content" />
+            <div className="media-right">
+              <p className="title is-3">£{total}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,41 +2,57 @@ import React, { useState } from "react";
 import "../styles/ProductCard.css";
 
 export const ProductCard = props => {
-  const { product } = props;
+  const { product, handleTotal } = props;
 
   const [count, setCount] = useState(0);
 
+  const addItem = () => {
+    setCount(count + 1);
+    handleTotal(product.price);
+  };
+
+  const removeItem = () => {
+    if (count > 0) {
+      setCount(count - 1);
+      handleTotal(-product.price);
+    }
+  };
+
+  const removeAll = () => {
+    handleTotal(-product.price * count);
+    setCount(0);
+  };
+
   return (
-    <div class="card product-card">
-      <div class="card-content">
-        <div class="media align-items-center">
-          <div class="media-left">
+    <div className="card product-card">
+      <div className="card-content">
+        <div className="media align-items-center">
+          <div className="media-left">
             <img
               src={product.img}
               alt="product image"
               className="product-img"
             />
           </div>
-          <div class="media-content">
-            <p class="title is-5">{product.name}</p>
-            <p class="subtitle is-5">£{product.price}</p>
+          <div className="media-content">
+            <p className="title is-5">{product.name}</p>
+            <p className="subtitle is-5">£{product.price}</p>
           </div>
-          <div className="media-right">
-            <button
-              class="button is-primary"
-              onClick={() => {
-                count > 0 && setCount(count - 1);
-              }}
-            >
+          <div className="media-right text-center">
+            <button className="button is-primary" onClick={removeItem}>
               -
             </button>
             <span className="title"> {count} </span>
-            <button
-              class="button is-primary"
-              onClick={() => setCount(count + 1)}
-            >
+            <button className="button is-primary" onClick={addItem}>
               +
             </button>
+            {count > 0 && (
+              <div>
+                <a className="has-text-primary" onClick={removeAll}>
+                  remove
+                </a>
+              </div>
+            )}
           </div>
         </div>
       </div>
