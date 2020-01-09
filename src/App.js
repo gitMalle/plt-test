1c4ffd2964f281915e9d7f47499470569f51983d
Daniel Malesevic <daniel.malesevic@gmx.at>
2020-01-09 15:01:24 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  // fetch all items
+  useEffect(() => {
+    const fetchItems = async () => {
+      const res = await axios.get('https://my-json-server.typicode.com/benirvingplt/products/products');
+      setItems(res.data);
+    }
+    fetchItems();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
